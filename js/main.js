0351f30afef55d6b25f7de6fd5db602e07a5e42f
Pegunito <artyomsasha@mail.ru>
2020-07-06 14:25:40 +0400

    убрал скролл при появлении модального окна и сделал стрелки слайдер pointer

--- js/main.js
+++ js/main.js
@@ -3,12 +3,19 @@
 
   const menu = document.querySelector('.hamburger-menu');
 
+  const scroll = document.querySelector('body')
+
   hamburger__icon.addEventListener('click', () => {
     hamburger__icon.classList.toggle("header__hamburger--active");
     menu.classList.toggle("hamburger-menu--active");
+    scroll.classList.toggle("body--active");
+
   });
   
 }());
+
+
+
 
 
 
