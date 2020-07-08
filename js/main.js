3061f27fe1cce4e8e9f55f5a720c48143f8a5ea2
Pegunito <artyomsasha@mail.ru>
2020-07-08 11:25:28 +0300

    добавил вертикальный аккордеон для декстоп

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+//hamburger
+
 (function () {
   const hamburger__icon = document.querySelector('.header__hamburger');
 
@@ -14,8 +16,36 @@
   
 }());
 
+//vertical acordeon
 
+const openItem = item => {
+  const container = item.closest(".member__item");
+  const contentBlock = container.find(".members__position");
+  const textBlock = contentBlock.find(".member__content");
+  const reqHeight = textBlock.height();
 
+  container.addClass("active");
+  contentBlock.height(reqHeight);
+}
 
+const closeEveryItem = container => {
+  const items = container.find('.members__position');
+  const itemContainer = container.find(".member__item");
 
+  itemContainer.removeClass("active");
+  items.height(0);
+}
 
+$('.member__title').click(e =>{
+  const $this = $(e.currentTarget);
+  const container = $this.closest('.team__list');
+  const elemContainer = $this.closest(".member__item");
+
+  if (elemContainer.hasClass("active")) {
+    closeEveryItem(container);
+  } else {
+    closeEveryItem(container);
+    openItem($this);
+  }
+
+});
