d0ddda85a9e25c2767443e1adf6823fac95a470c
Pegunito <artyomsasha@mail.ru>
2020-07-21 07:29:33 +0300

    добавил стилей для map и video

--- js/main.js
+++ js/main.js
@@ -9,7 +9,28 @@ const init = () => {
     controls: []
   });
 
+  const coords = [
+    [59.94554327989287, 30.38935262114668],
+    [59.91142323563909, 30.50024587065841],
+    [59.88693161784606, 30.319658102103713],
+    [59.97033574821672, 30.315194906302924],
+  ];
 
+  const myCollection = new ymaps.GeoObjectCollection({}, {
+    draggable: false,
+    iconLayout: 'default#image',
+    iconImageHref: "./img/icons/Ymarker.svg",
+    iconImageSize: [46, 57],
+    iconImageOffset: [-35, -52]
+  });
+
+  for (let i = 0; i < coords.length; i++) {
+    myCollection.add(new ymaps.Placemark(coords[i]));
+  }
+  
+  myMap.geoObjects.add(myCollection);
+  
+  myMap.behaviors.disable('scrollZoom');
 };
 
 ymaps.ready(init);
